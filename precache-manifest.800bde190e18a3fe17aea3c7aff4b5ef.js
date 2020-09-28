bf1afdade14c91742640a161b851df3d3756756f
Giuseppe <ggmele1@gmail.com>
2020-09-28 14:33:15 -0700

    Updates

---
+++ precache-manifest.800bde190e18a3fe17aea3c7aff4b5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb16ba505f5a90a56bfe0030c155ffc7",
+    "url": "/spotify-stats/index.html"
+  },
+  {
+    "revision": "e5afd20c0aa8234fe168",
+    "url": "/spotify-stats/static/css/main.3ab8875a.chunk.css"
+  },
+  {
+    "revision": "f52075285c9c6a96385d",
+    "url": "/spotify-stats/static/js/2.9ba17319.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-stats/static/js/2.9ba17319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5afd20c0aa8234fe168",
+    "url": "/spotify-stats/static/js/main.d091b8b9.chunk.js"
+  },
+  {
+    "revision": "1c8373c3ba4a4de54373",
+    "url": "/spotify-stats/static/js/runtime-main.1f1c932b.js"
+  }
+]);
